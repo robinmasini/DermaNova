a860b16e52973e956d345d5c16550d31e218ad76
Robin MASINI <robinmasini@MacBook-Air-RM.local>
2026-07-16 17:03:22 +0200

    fix(pwa): correctly configured PWA with precise sizes and grey icon

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'dermanova-v5';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/dn-pwa-icon.png',
+        '/dn-pwa-192.png',
+        '/dn-pwa-512.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
